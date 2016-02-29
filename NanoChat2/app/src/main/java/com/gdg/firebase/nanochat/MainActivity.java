5d0fd53e8717923fe609bc385f25b2de0619e550
khacpham <khacpv@gmail.com>
2016-02-29 20:58:34 +0700

    Step 3.3: handle send message event

--- NanoChat2/app/src/main/java/com/gdg/firebase/nanochat/MainActivity.java
+++ NanoChat2/app/src/main/java/com/gdg/firebase/nanochat/MainActivity.java
@@ -2,10 +2,14 @@ package com.gdg.firebase.nanochat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.firebase.client.Firebase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText textEdit = (EditText) this.findViewById(R.id.text_edit);
         Button sendButton = (Button) this.findViewById(R.id.send_button);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = textEdit.getText().toString();
+                Map<String,Object> values = new HashMap<>();
+                values.put("name", "Android User");
+                values.put("text", text);
+                mFirebaseRef.push().setValue(values);
+                textEdit.setText("");
+            }
+        });
     }
 }
