7796b0ded4c07941a6652753c11e381b5b385868
khacpham <khacpv@gmail.com>
2016-02-29 21:05:52 +0700

    Step 4.3: create ListView controller and its adapter

--- NanoChat2/app/src/main/java/com/gdg/firebase/nanochat/MainActivity.java
+++ NanoChat2/app/src/main/java/com/gdg/firebase/nanochat/MainActivity.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.firebase.client.Firebase;
+import com.firebase.ui.FirebaseListAdapter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +17,7 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     private Firebase mFirebaseRef;
+    FirebaseListAdapter<ChatMessage> mListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +41,16 @@ public class MainActivity extends AppCompatActivity {
                 textEdit.setText("");
             }
         });
+
+        final ListView listView = (ListView) this.findViewById(android.R.id.list);
+        mListAdapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
+                android.R.layout.two_line_list_item, mFirebaseRef) {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                ((TextView)v.findViewById(android.R.id.text1)).setText(model.getName());
+                ((TextView)v.findViewById(android.R.id.text2)).setText(model.getText());
+            }
+        };
+        listView.setAdapter(mListAdapter);
     }
 }
